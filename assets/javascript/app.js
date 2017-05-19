23f686853fa584fea7e6de804785bd8a912f2e2e
Fang <mr.alan.tran@gmail.com>
2017-05-18 18:33:36 -0700

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+    var topics = ["Archer", "Futurama", "Minions", "Spongebob", "Simpsons", "Garfield", "Rugrats"];
+
+    $.each(topics, function(i, val) {
+        var topicButton = $('<button>').attr('class', 'btn').addClass('btn-primary').text(val);
+        $('.buttons').append(topicButton);
+    });
+
+    // whenever a button is clicked
+    // query the subject
+    // and populate gifs
+    $('.buttons .btn-primary').on('click', function() {
+        console.log($(this).text());
+
+        var topicFromButton = $(this).text();
+        getGifs(topicFromButton);
+
+    });
+
+    $(document).on('click', '.gif-image', function() {
+        var stateAttr = $(this).attr('data-state');
+
+        if (stateAttr === "still") {
+            $(this).attr('src', $(this).attr('data-animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr('data-still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+
+    // takes an array of gif objects
+    // and creates elements in the HTML in gif-area
+    function populateGifs(array) {
+        console.log('populateGifs');
+        console.log(array);
+
+        var gifArea = $('.gif-area');
+        gifArea.empty();
+
+        $('<div>')
+
+        $.each(array, function(i, val) {
+            gifArea.append($('<div>', {class: 'gif-container'}).append($('<div>').text("Rating: " + val.rating))
+                .append($('<img>')
+                .attr('src', val.images.fixed_height_small_still.url)
+                .attr('data-still', val.images.fixed_height_small_still.url)
+                .attr('data-animate', val.images.fixed_height_small.url)
+                .attr('data-state', 'still')
+                .addClass('gif-image')));
+        });
+    }
+
+    // queries the API with the subject
+    // and calls populatesGifs array
+    function getGifs(subject) {
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+            subject + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        console.log(queryURL);
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .done(function(response) {
+
+                var gifArray = [];
+
+                $.each(response.data, function(i, val) {
+
+                    gifArray = response.data;
+                });
+
+                populateGifs(gifArray);
+
+            });
+    }
+
+
+});
