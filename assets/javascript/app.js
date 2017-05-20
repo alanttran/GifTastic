653164f8b2d6899d85f92800b573ff721ae36b0a
alanttran <mr.alan.tran@gmail.com>
2017-05-19 22:11:19 -0700

    https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
     // queries the API with the subject
     // and calls populatesGifs array
     function getGifs(subject) {
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             subject + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         console.log(queryURL);
